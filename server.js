d6009b7a21e0be63b0d4d47ff95e081f10b3839d
abdelrahman arfat <abdoarfat924@gmail.com>
2025-01-31 11:48:25 +0200

    - finish category crud - add Error handling module to all project -

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 import express from "express";
 import { dbConnection } from "./database/dbConnection.js";
 import { bootstrap } from "./src/modules/category/bootstrap.js";
+import { globalError } from "./src/middleware/globalError.js";
+import { AppError } from "./src/utils/appError.js";
 const app = express();
 const port = 3000;
-
 app.use(express.json())
 bootstrap(app)
+ app.use('*',(req,res,next)=>{
+
+next(new AppError(`route not found : ${req.originalUrl}`,404))
+
+ })
+ app.use(globalError)
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 })
--- src/modules/category/category.controller.js
+++ src/modules/category/category.controller.js
@@ -1,46 +1,52 @@
 import slugify from "slugify"
 import { Category } from "../../../database/models/category.model.js"
+import { AppError } from "../../utils/appError.js"
+import { catchError } from "../../middleware/catchError.js"
 
-const addCategory =async (req,res,next)=>{
-req.body.slug= slugify(req.body.name)
-let category=new Category(req.body)
-await category.save()
+const addCategory =catchError(async (req,res,next)=>{
+    req.body.slug= slugify(req.body.name)
+    let category=new Category(req.body)
+    await category.save()
+    
+    res.json({message:"success",category })
+    
+    })
 
-res.json({message:"success",category })
 
-}
 
-const allCategories =async (req,res,next)=>{
+const allCategories =catchError(async (req,res,next)=>{
     let categories=await Category.find()
     
     res.json({message:"success",categories })
     
-    }
+    })
 
 
-    const getCategory =async (req,res,next)=>{
+    const getCategory =catchError(async (req,res,next)=>{
         let category=await Category.findById(req.params.id)
         
-        res.json({message:"success",category})
+        category ||   next(new AppError("category not found",404))
+        !category || res.json({message:"success",category})
         
-        }
+        })
 
-        const updateCategory =async (req,res,next)=>{
+        const updateCategory =catchError(async (req,res,next)=>{
             req.body.slug= slugify(req.body.name)
 
             let category=await Category.findByIdAndUpdate(req.params.id,req.body,{new:true})
             
-            res.json({message:"success",category})
+            category || next(new AppError("category not found",404))
+            !category || res.json({message:"success",category})
             
-            }
+            })
 
             
-        const deleteCategory =async (req,res,next)=>{
+        const deleteCategory =catchError(async (req,res,next)=>{
             let category=await Category.findByIdAndDelete(req.params.id)
+            category ||   next(new AppError("category not found",404))
+            !category || res.json({message:"success",category})
             
-            res.json({message:"success",category})
-            
-            }
+            })
     
 
 export {
